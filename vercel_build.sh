3e5ba793af9addabcdeddc1d39d5829aa214487c
Sulthon Abiyyu <msulthonabiyyu108@gmail.com>
2025-11-24 00:30:53 +0700

    new build.sh

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,27 +1,16 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
 
-# 1) install / update flutter SDK in workspace
-if [ -d flutter ]; then
+# Clone Flutter jika belum ada
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git
+else
   cd flutter
   git pull
   cd ..
-else
-  git clone https://github.com/flutter/flutter.git
 fi
 
-# 2) add flutter to PATH for this session
 export PATH="$(pwd)/flutter/bin:$PATH"
 
-# 3) enable web support (idempotent)
 flutter config --enable-web
-
-# 4) go to project directory (jika app di subfolder, ganti '.' jadi 'namafolder')
-cd .
-
-# 5) get dependencies and build web
 flutter pub get
-flutter build web --release
-
-# 6) show build result
-ls -la build/web
+flutter build web
